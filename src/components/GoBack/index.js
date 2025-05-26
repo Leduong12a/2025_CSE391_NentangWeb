151e4110a6ac40e0e3455b262f412b6305b16b35
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-05-26 10:40:04 +0700

    update-product-item

--- src/components/GoBack/index.js
+++ src/components/GoBack/index.js
@@ -1,15 +1,17 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa6";
-import "./index.css"
-function GoBack(){
-    const navigate = useNavigate();
-    const handleClick = ()=>{
-        navigate(-1);
-    }
-    return(
-        <>
-        <button onClick={handleClick} className="button"><FaArrowLeft /></button>
-        </>
-    )
+import "./index.css";
+function GoBack() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(-1);
+  };
+  return (
+    <>
+      <button onClick={handleClick} className="button">
+        <FaArrowLeft />
+      </button>
+    </>
+  );
 }
-export default GoBack
+export default GoBack;
